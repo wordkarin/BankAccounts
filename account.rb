26341af83f9ba60259fed3fab7568637e9beeb55
Karin Kubischta <karin.kubischta@gmail.com>
2016-08-24 12:20:27 -0700

    updating init method to require account_id as an argument, rather than generating it itself.

--- account.rb
+++ account.rb
@@ -1,5 +1,6 @@
 require 'money' #not using this yet, but should!
 require_relative 'owner'
+require 'csv'
 
 # Create a Bank module which will contain your Account class and any future bank account logic.
 module Bank
@@ -9,14 +10,15 @@ module Bank
     attr_reader :account_id, :balance, :owner_id
 
     # A new account should be created with an ID and an initial balance
-    def initialize(balance = 0)
-      @account_id = rand(100000..999999) #TODO: does not currently ensure unique number.
+    def initialize(account_id, balance = 0)
+      # @account_id = rand(100000..999999) #TODO: does not currently ensure unique number. I will probably have the account_id's created somewhere else outside of the initialize method, so that I can keep track of them and ensure that they're unique. for now, I'll update the initialize method to take in both the account number and the balance.
+
       if balance >= 0 #TODO: if user enters a non-number for balance, this fails.
         @balance = balance
       else
         begin
           # A new account cannot be created with initial negative balance - this will raise an ArgumentError
-          raise neg_starting_balance = ArgumentError.new("You cannot create an account with a negative balance. Your account was created with a balance of 0. If you meant to create an account with a positive balance, please deposit now.")
+          raise neg_starting_balance = ArgumentError.new("You cannot create an account with a negative balance. Your account was created with a balance of 0. If you meant to create an account with a positive balance, please make a deposit now.")
         rescue
           puts neg_starting_balance.message
           @balance = 0 #This rescue creates the account object with a zero balance. Maybe want to not create an account if the balance is negative?
@@ -71,7 +73,7 @@ module Bank
 
     # The Account can be created with an owner, OR you can create a method that will add the owner after the Account has already been created.
     def add_owner(owner)
-        @owner = owner #do I need to figure out how to associate this with the current instance of the account? (would call this method on an account instance.) 
+        @owner = owner #do I need to figure out how to associate this with the current instance of the account? (would call this method on an account instance.)
     end
   end
 end
