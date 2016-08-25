62b931873e79cf3f96bac33d556fe9de375b8223
Karin Kubischta <karin.kubischta@gmail.com>
2016-08-25 15:17:38 -0700

    added a fee parameter to withdraw method

--- account.rb
+++ account.rb
@@ -6,9 +6,9 @@ require_relative 'owner'
 module Bank
   # Create an Account class which should have the following functionality:
   class Account
+    @@min_balance = 0 #this is valid across all instances of the class.
     # Should be able to access the current balance of an account at any time.
     attr_reader :account_id, :balance, :owner_id
-    @@min_balance = 0
 
     # A new account should be created with an ID and an initial balance
     def initialize(account_id, open_date, balance)
@@ -17,15 +17,16 @@ module Bank
       @account_id = account_id
       @open_date = open_date
       #TODO: I'm not doing anything with open_date yet, but the data from accounts.csv has it in there, and I may want to initilaize with a default open_date of now.
-
+      puts @@min_balance
       if balance >= @@min_balance #TODO: if user enters a non-number for balance, this fails.
         @balance = balance
       else
         begin
           # A new account cannot be created with balance lower than the minimum - this will raise an ArgumentError
-          raise ArgumentError.new("You cannot create an account with a balance below #{ @@min_balance }. Please make a deposit of #{ @@min_balance } or greater.")
-        rescue Exception => error #The problem with this argument error/rescue block is that if someone tries to create an account with a lower than min balance, the method still executes with the min balance
-          puts error.message
+          raise ArgumentError.new("You cannot create an account with a balance below #{ @@min_balance }.")
+        # rescue Exception => error
+        #   puts error.message
+          # The problem with this argument error/rescue block is that if someone tries to create an account with a lower than min balance, and I want to rescue it but not create the account. For now I'll just throw the exception and not rescue, so that I don't end up with accounts with no balance. Another option is to rescue and set the balance to 0 and tell the user to immediately deposit the min balance.
         end
       end
     end
@@ -57,19 +58,21 @@ module Bank
     end
 
     # Should have a withdraw method that accepts a single parameter which represents the amount of money that will be withdrawn. This method should return the updated account balance.
-    def withdraw(amount)
+    def withdraw(amount, fee = 0)
+      # If we set a default fee to be 0, then we can change the fee based on what kind of account/withdrawl is made.
       # The withdraw method does not allow the account to go negative - Will output a warning message and return the original un-modified balance
       # Withdraw should also not accept a negative number as the amount.
+      total_withdrawl = amount + fee
 
-      if amount > 0 && amount <= @balance
-        @balance -= amount
+      if amount > 0 && total_withdrawl + @@min_balance <= @balance
+        @balance -= total_withdrawl
       elsif amount < 0
         begin
           raise neg_withdraw = ArgumentError.new("You cannot withdraw a negative amount.")
         rescue
           puts neg_withdraw.message
         end
-      elsif amount > @balance
+      elsif total_withdrawl + @@min_balance > @balance
         begin
           raise amount_exceeds_balance = ArgumentError.new("You do not have enough money to withdraw #{amount}.")
         rescue
--- savings_account.rb
+++ savings_account.rb
@@ -1,7 +1,7 @@
 require_relative 'account'
 module Bank
   class SavingsAccount < Account
-    @@min_balance = 1000
+    @@min_balance = 1000 #this is valid across all instances of this class.
 
     def initialize(account_id, open_date, amount, interest_rate)
       # The initial balance cannot be less than $10. If it is, this will raise an ArgumentError
@@ -10,8 +10,11 @@ module Bank
     end
 
     # Updated withdrawal functionality:
-    # Each withdrawal 'transaction' incurs a fee of $2 that is taken out of the balance.
-    # Does not allow the account to go below the $10 minimum balance - Will output a warning message and return the original un-modified balance
+    def withdraw(amount)
+      super(amount, 200)
+      # Each withdrawal 'transaction' incurs a fee of $2 that is taken out of the balance; we're passing 200 ($2) as a parameter to Account's withdraw method, where the default is 0, since every withdrawl from a savings account incurs a $2 fee.
+      # The Account class's withdraw method does not allow the account to go below the class's minimum balance - Will output a warning message and return the original un-modified balance. For SavingsAccount's, this is 1000 ($10). 
+    end
 
     # It should include the following new method:
     # #add_interest(rate): Calculate the interest on the balance and add the interest to the balance. Return the interest that was calculated and added to the balance (not the updated balance).
